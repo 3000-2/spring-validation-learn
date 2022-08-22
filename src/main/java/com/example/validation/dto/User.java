3da4aeeb8b19bf628e9e606e74062d531f2b9aeb
3000-2 <psch0493@gmail.com>
2022-08-22 22:01:34 +0900

    feature: validation test

---
+++ src/main/java/com/example/validation/dto/User.java
@@ -0,0 +1,61 @@
+package com.example.validation.dto;
+
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.*;
+
+public class User {
+
+    @NotBlank
+    @Length(min = 2)
+    private String name;
+
+    @Max(value = 100)
+    private int age;
+    @Email
+    private String email;
+    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "번호 양식과 맞지 않습니다. 000-0000-0000")
+    private String phoneNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+}
